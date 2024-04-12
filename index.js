ebb24de4d9e15c2e004cdfb6f26bc8a6e1fd8d97
Vinod Pahuja <vinod.pahuja@gmail.com>
2024-04-12 19:48:34 +0530

    Added CSG example

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 
-//var TI_BASE = 'https://threeinventor.github.io/';
-var TI_BASE = 'http://localhost:8080/threeinventor.github.io';
+var TI_BASE = 'https://threeinventor.github.io/';
+//var TI_BASE = 'http://localhost:8080/TI/threeinventor.github.io';
 
 function openInTI(id) {
 	window.open(TI_BASE + '?id=' + id);
 }
 
-if(ws) ws.initWorkspace({ readOnly: true});
+window.onload = async function() {
+	initTHREE();
+	if(ws) ws.init({ readOnly: true});
+}
